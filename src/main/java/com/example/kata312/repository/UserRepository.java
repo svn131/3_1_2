a3e29a588abee1b47d8d111a04dae7a55eb47746
Vladimir Solovyov <svn131131@gmail.com>
2023-04-23 23:44:43 +0500

    Ready

---
+++ src/main/java/com/example/kata312/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.kata312.repository;
+
+import com.example.kata312.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository  extends JpaRepository<User,Long> {
+    List<User> findByLastName (String lastName);
+}
